bf8cf0a4647a42a4756f385bb9134874c6a48800
fionabrendel <fiona.brendel@gmail.com>
2022-12-01 15:51:23 +0100

    Kapitel 1

--- Capicity/Capicity.cpp
+++ Capicity/Capicity.cpp
@@ -1,20 +1,173 @@
-// Capicity.cpp : Diese Datei enthält die Funktion "main". Hier beginnt und endet die Ausführung des Programms.
-//
-
 #include <iostream>
+#include <cstdlib>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+enum gebaeudetype { empty, TYP1, TYP2, TYP3 };
+bool running = true;
+int input = 0;
+int bbreite = 0;
+int bhoehe = 0;
+gebaeudetype** bereich;
+int posx = 0;
+int posy = 0;
+int hoehe = 0;
+int breite = 0;
+
+//prüft beim Bau eines Gebäudes auf Kollisionen mit andern, bzw ob außerhalb des Bereichs
+bool checkKollision(int hoehe, int breite, int posx, int posy) {
+    //außerhalb des Bereichs?
+    if (posy >= bhoehe || posx >= bbreite || hoehe + posy >= bhoehe || breite + posx >= bbreite) {
+        return false;
+    }
+    //Kollision mit anderem Gebäude?
+    for (int i = posy; i < posy + hoehe; i++) {
+        for (int j = posx; j < posx + breite; j++) {
+            if (bereich[i][j] != 0)
+                return false;
+        }
+    }
+
+    return true;
 }
 
-// Programm ausführen: STRG+F5 oder Menüeintrag "Debuggen" > "Starten ohne Debuggen starten"
-// Programm debuggen: F5 oder "Debuggen" > Menü "Debuggen starten"
+//plaziert ein Gebäude im Bereich
+void placeGebaeude() {
+    enum gebaeudetype art;
+    cout << "Welche Art von Gebaeude? 1: Holz, 2: Stein, 3: Sand" << endl;;
+    cin >> input;
+    switch (input)
+    {
+    case 1: 	art = TYP1;
+        break;
+    case 2: 	art = TYP2;
+        break;
+    case 3: 	art = TYP3;
+        break;
+    default:
+        cout << "Ungueltiger Gebaeudetyp" << endl;
+        return;
+    }
+    cout << "Position x:";
+    cin >> posx;
+    cout << "Position y:";
+    cin >> posy;
+    cout << "Hoehe:";
+    cin >> hoehe;
+    cout << "Breite:";
+    cin >> breite;
+    if (posx < 0 || posy < 0 || hoehe < 0 || breite < 0) {
+        cout << "Ungueltige Eingabe" << endl;
+        return;
+    }
+    if (checkKollision(hoehe, breite, posx, posy))
+    {
+        for (int i = posy; i < posy + hoehe; i++) {
+            for (int j = posx; j < posx + breite; j++) {
+                bereich[i][j] = art;
+            }
+        }
+        cout << "Gebaeude wurde plaziert" << endl;
+    }
+    else {
+        cout << "Gebaeude kann nicht plaziert werden" << endl;
+    }
+}
 
-// Tipps für den Einstieg: 
-//   1. Verwenden Sie das Projektmappen-Explorer-Fenster zum Hinzufügen/Verwalten von Dateien.
-//   2. Verwenden Sie das Team Explorer-Fenster zum Herstellen einer Verbindung mit der Quellcodeverwaltung.
-//   3. Verwenden Sie das Ausgabefenster, um die Buildausgabe und andere Nachrichten anzuzeigen.
-//   4. Verwenden Sie das Fenster "Fehlerliste", um Fehler anzuzeigen.
-//   5. Wechseln Sie zu "Projekt" > "Neues Element hinzufügen", um neue Codedateien zu erstellen, bzw. zu "Projekt" > "Vorhandenes Element hinzufügen", um dem Projekt vorhandene Codedateien hinzuzufügen.
-//   6. Um dieses Projekt später erneut zu öffnen, wechseln Sie zu "Datei" > "Öffnen" > "Projekt", und wählen Sie die SLN-Datei aus.
+//löscht einen Bereich
+void deleteBereich() {
+    cout << "Position x:";
+    cin >> posx;
+    cout << "Position y:";
+    cin >> posy;
+    cout << "Breite nach rechts:";
+    cin >> hoehe;
+    cout << "Hoehe nach unten y:";
+    cin >> breite;
+
+    if (posx < 0 || posy < 0 || hoehe < 0 || breite < 0) {
+        cout << "Ungueltige Eingabe" << endl;
+        return;
+    }
+
+    if (posx < bbreite && posy < bhoehe && posx + breite < bbreite && posy + hoehe < bhoehe) {
+        for (int i = posy; i < posy + hoehe; i++) {
+            for (int j = posx; j < posx + breite; j++) {
+                bereich[i][j] = gebaeudetype(0);
+            }
+        }
+    }
+    else {
+        cout << "Position außerhalb des Bereichs" << endl;
+    }
+}
+
+//gibt den Bauplan aus
+void printBauplan() {
+    for (int i = 0; i < bbreite; i++)
+    {
+        for (int j = 0; j < bhoehe; j++)
+        {
+            cout << bereich[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+}
+
+//beendet das Programm
+void stop() {
+    cout << "Programm wird beendet" << endl;
+    running = false;
+}
+
+//zeigt ein Menü an, mit dem die Funktionen aufgerufen werden können
+void showMenu() {
+    cout << "-----------------------" << endl;
+    cout << "Waehle eine Aktion:" << endl;
+    cout << "1: Gebaeude setzen" << endl;
+    cout << "2: Bereich loeschen" << endl;
+    cout << "3: Bauplan ausgeben" << endl;
+    cout << "4: Programm beenden" << endl;
+    cin >> input;
+    switch (input)
+    {
+    case 1:
+        placeGebaeude();
+        break;
+    case 2:
+        deleteBereich();
+        break;
+    case 3:
+        printBauplan();
+        break;
+    case 4:
+        stop();
+        break;
+    default:
+        cout << "Fehlerhafte Eingabe!" << endl;
+        break;
+    }
+}
+
+//Startmethode
+int main(int argc, char* argv[]) {
+    bbreite = strtol(argv[1], nullptr, 10);
+    bhoehe = strtol(argv[2], nullptr, 10);
+
+    bereich = new gebaeudetype * [bhoehe];
+    for (int i = 0; i < bhoehe; i++)
+    {
+        bereich[i] = new gebaeudetype[bbreite];
+    }
+
+    for (int i = 0; i < bhoehe; i++) {
+        for (int j = 0; j < bbreite; j++) {
+            bereich[i][j] = gebaeudetype(0);
+        }
+    }
+    while (running)
+    {
+        showMenu();
+    }
+
+}
